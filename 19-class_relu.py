c558e1d130b98a76966d33ef44f6408a1fcac7ab
jhoanmartinez <dsa.jhoan@gmail.com>
2021-01-04 23:58:17 -0500

    relu

--- 19-class_relu.py
+++ 19-class_relu.py
@@ -4,5 +4,5 @@ class Activation_Relu:
 
     # Forward pass
     def forward(self, inputs):
-    # Calculate output values from input
-    self.output = np.maximum(0, inputs)
+	    # Calculate output values from input
+	    self.output = np.maximum(0, inputs)
--- 20-1-activation.py
+++ 20-1-activation.py
@@ -1,29 +1,31 @@
 import numpy as np
 
-inputs = np.array([
-	[-1,-1],
-	[-1,4],
+X = np.array([
+	[3, -6],
+	[-3, 5],
+	[7, -4],
+	[2, -2],
+	[8, -2],
+	[9, -34]
 ])
 
 class Layer_Dense:
-	def __init__(self, n_inputs, n_neurons):
-		self.weights = 0.1 * np.random.rand(n_inputs, n_neurons)
-		self.biases = np.zeros((1, n_neurons))
+	def __init__(self, n_inputs, n_neurones):
+		self.weights = 0.1 * np.random.randn(n_inputs, n_neurones)
+		self.biases = np.zeros((1, n_neurones))
 
-	def forward(self, inputs):
-		print("NO ACTIVATION")
-		self.outputs = np.dot(inputs, self.weights) + self.biases
-		return self.outputs
-
-hidden_1 = Layer_Dense(2, 3)
-print(hidden_1.forward(inputs))
+	def forward_layer(self, inputs):
+		self.output = np.dot(inputs, self.weights) + self.biases
+		return self.output
 
 class Activation_ReLU:
-	def activate(self, inputs):
-		print("\nActivation_ReLU")
-		self.acti_out = np.maximum(0, inputs)
+	def forward_relu(self, input):
+		self.acti_out = np.maximum(0, input)
 		return self.acti_out
 
-acti_hidden = Activation_ReLU()
-acti_hidden.activate(hidden_1.outputs)
-print(acti_hidden.activate(hidden_1.outputs))
+hidden_1 = Layer_Dense(2, 3)
+h1 = hidden_1.forward_layer(X)
+
+activation1 = Activation_ReLU()
+act1 = activation1.forward_relu(h1)
+print(act1[:5])
