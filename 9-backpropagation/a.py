7ac77e9760b580bd66cbbba0ffb1a81f56184579
jhoanmartinez <dsa.jhoan@gmail.com>
2021-06-08 10:34:36 -0500

    chain rule backpropagation

--- 9-backpropagation/a.py
+++ 9-backpropagation/a.py
@@ -8,105 +8,48 @@ b = 1.0
 xw0 = x[0]*w[0]
 xw1 = x[1]*w[1]
 xw2 = x[2]*w[2]
-print("values=",xw0, xw1, xw2, b)
+print("multi =",xw0, xw1, xw2, "bias=>",b)
 
 #adding biase
 z = xw0 + xw1 + xw2 + b
-print("neuron=",z)
+print("multi mas bias 1 =",z)
 
 #ReLU activation
 y = max(z, 0)
-print("reulu out=",y)
+print("reulu 1 out =",y)
 
-# derivate ReLU
-drelu_dz = (1.0 if z > 0.0 else 0.0)
-print("drelu_dz first=",drelu_dz)
-
-# derivate from the next layer
+#Derivada de adelante por el valor de relu
 dvalue = 1.0
 
-# Derivative of ReLU and the chain rule
-drelu_dz = dvalue * (1. if z > 0 else 0.)
-print("chain rule=",drelu_dz)
+#derivada de relu y chain rule
+drelu_dz = dvalue * (1 if y > 0 else 0)
+print("drelu dz =",drelu_dz)
 
-# Partial derivatives of the sum, the chain rule
-dsum_dxw0 = 1
-drelu_dxw0 = drelu_dz * dsum_dxw0
-print(drelu_dxw0)
+# For the first partial derivative:
+dsum_dxw0, dsum_dxw1, dsum_dxw2, dsum_db = 1, 1, 1, 1
 
-# Partial derivatives of the sum, the chain rule
-dsum_dxw0 = 1
-dsum_dxw1 = 1
-dsum_dxw2 = 1
-dsum_db = 1
 drelu_dxw0 = drelu_dz * dsum_dxw0
 drelu_dxw1 = drelu_dz * dsum_dxw1
 drelu_dxw2 = drelu_dz * dsum_dxw2
 drelu_db = drelu_dz * dsum_db
-print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
 
-# Partial derivatives of the multiplication, the chain rule
+print("dz * Xn+b =",drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
+
+#derivawada de input X0*W0 * derivada entrante (drelu_dxwn)
 dmul_dx0 = w[0]
-dmul_dx1 = w[1]
-dmul_dx2 = w[2]
 dmul_dw0 = x[0]
+dmul_dx1 = w[1]
 dmul_dw1 = x[1]
+dmul_dx2 = w[2]
 dmul_dw2 = x[2]
-drelu_dx0 = drelu_dxw0 * dmul_dx0
-drelu_dw0 = drelu_dxw0 * dmul_dw0
-drelu_dx1 = drelu_dxw1 * dmul_dx1
-drelu_dw1 = drelu_dxw1 * dmul_dw1
-drelu_dx2 = drelu_dxw2 * dmul_dx2
-drelu_dw2 = drelu_dxw2 * dmul_dw2
-print(drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
+
+drelu_x0 = (drelu_dz * dsum_dxw0) * dmul_dx0
+drelu_w0 = (drelu_dz * dsum_dxw0) * dmul_dw0
+drelu_x1 = (drelu_dz * dsum_dxw1) * dmul_dx1
+drelu_w1 = (drelu_dz * dsum_dxw1) * dmul_dw1
+drelu_x2 = (drelu_dz * dsum_dxw2)* dmul_dx2
+drelu_w2 = (drelu_dz * dsum_dxw2)* dmul_dw2
+
+print(drelu_x0, drelu_w0, drelu_x1, drelu_w1, drelu_x2, drelu_w2)
 
 
-dx = [drelu_dx0, drelu_dx1, drelu_dx2]  # gradients on inputs
-dw = [drelu_dw0, drelu_dw1, drelu_dw2]  # gradients on weights
-db = drelu_db  # gradient on bias...just 1 bias here.
-
-print(w, b)
-
-w[0] += -0.001 * dw[0]
-w[1] += -0.001 * dw[1]
-w[2] += -0.001 * dw[2]
-b += -0.001 * db
-
-print(w, b)
-
-# Multiplying inputs by weights
-xw0 = x[0] * w[0]
-xw1 = x[1] * w[1]
-xw2 = x[2] * w[2]
-
-# Adding
-z = xw0 + xw1 + xw2 + b
-
-# ReLU activation function
-y = max(z, 0)
-print(y)
-
-dx = [drelu_dx0, drelu_dx1, drelu_dx2]  # gradients on inputs
-dw = [drelu_dw0, drelu_dw1, drelu_dw2]  # gradients on weights
-db = drelu_db
-
-print("w-b",w, b)
-
-# We can then apply a fraction of the gradients to these values:
-w[0] += -0.001 * dw[0]
-w[1] += -0.001 * dw[1]
-w[2] += -0.001 * dw[2]
-b += -0.001 * db
-print(w, b)
-
-# Multiplying inputs by weights
-xw0 = x[0] * w[0]
-xw1 = x[1] * w[1]
-xw2 = x[2] * w[2]
-
-# Adding
-z = xw0 + xw1 + xw2 + b
-
-# ReLU activation function
-y = max(z, 0)
-print("max=",y)
--- 9-backpropagation/i.py
+++ 9-backpropagation/i.py
@@ -38,3 +38,48 @@ class Activation_ReLU:
 
         # Zero gradient where input values were negative
         self.dinputs[self.inputs <= 0] = 0
+
+# Cross-entropy loss
+class Loss_CategoricalCrossentropy(Loss):
+    
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+        # Number of labels in every sample
+        # We'll use the first sample to count them
+        labels = len(dvalues[0])
+
+        # If labels are sparse, turn them into one-hot vector
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        # Calculate gradient
+        self.dinputs = -y_true / dvalues
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+# Softmax activation
+class Activation_Softmax:
+
+# Backward pass
+    def backward(self, dvalues):
+
+        # Create uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in \
+                enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - \
+                              np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix,
+                                         single_dvalues)
+
+
